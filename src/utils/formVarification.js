e2f9fa1fcba820a4fe99be70e92ed2d3ebadc110
JulesEfrei <jules.bruzeau@efrei.net>
2023-02-19 17:35:40 +0000

    Add : Form Verification function

---
+++ src/utils/formVarification.js
@@ -0,0 +1,60 @@
+export function isText(name) {
+  if (typeof name !== "undefined") {
+    return name.trim() !== "" && !name.match(/[0-9]/gm);
+  } else {
+    return false;
+  }
+}
+
+export function isPassword(password) {
+  if (typeof password !== "undefined") {
+    return password.toLowerCase() !== password && password.length >= 8;
+  } else {
+    return false;
+  }
+}
+
+export function isEmail(email) {
+  if (typeof email !== "undefined") {
+    return email.match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/)
+      ? true
+      : false;
+  } else {
+    return false;
+  }
+}
+
+export function isPhone(tel) {
+  if (typeof tel !== "undefined") {
+    return tel.match(
+      /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?/
+    );
+  }
+}
+
+export function isSelect(defaultValue, selected) {
+  if (defaultValue === selected) return false;
+  return true;
+}
+
+const valueWithCheck = {
+  name: isText,
+  email: isEmail,
+  password: isPassword,
+  phone: isPhone,
+  select: isSelect,
+};
+
+export function verifForm(field) {
+  let result = true;
+  let error = [];
+
+  Object.keys(field).forEach((elm) => {
+    if (!!field.elm || !valueWithCheck[elm](field[elm])) {
+      result = false;
+      error.push(elm);
+    }
+  });
+
+  return { success: result, error };
+}
