f739e0f53d6b396de8b7850922e388cce23b9c01
JulesEfrei <jules.bruzeau@efrei.net>
2023-02-21 11:45:24 +0000

    Fix : Input Style

--- src/components/Pages/LoginPage.jsx
+++ src/components/Pages/LoginPage.jsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Input } from "../Atoms/";
 
 function LoginPage() {
   const inputRef = useRef(null);
-
-  console.log(inputRef.current);
-  useEffect(() => {
-    console.log(inputRef.current);
-  }, [inputRef]);
 
   return (
     <>
@@ -18,7 +13,6 @@ function LoginPage() {
         label="Password"
         type="password"
         ref={inputRef}
-        value={inputRef.current ? inputRef.current.value : ""}
       />
     </>
   );
