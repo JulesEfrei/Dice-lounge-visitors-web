cc28c0bcf6ee5de30fd03f06502fb1590b1a6b93
JulesEfrei <jules.bruzeau@efrei.net>
2023-03-03 14:39:32 +0000

    Add : Value verification

--- src/components/Sections/BookingInfo.jsx
+++ src/components/Sections/BookingInfo.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./styles/bookingInfo.module.scss";
 import Picker from "react-mobile-picker-mod";
+import { generateToast } from "../../utils/formVarification";
 
-function BookingInfo() {
+function BookingInfo({ updateData, goForward, goBack }) {
   const [nbPeople, setNbPeople] = useState(2);
+  const selectRef = useRef(null);
 
   const hours = new Array(12)
-    .fill(0, 0)
+    .fill(0, "0")
     .map((elm, index) => (index + 1).toString());
   const minutes = new Array(60)
-    .fill(0, 0)
-    .map((elm, index) => index.toString());
+    .fill(0, "0")
+    .map((elm, index) =>
+      index < 10 ? elm + index.toString() : index.toString()
+    );
 
   const [optionGroups, setOptionGroups] = useState({
     hours,
@@ -19,8 +23,8 @@ function BookingInfo() {
   });
 
   const [valueGroups, setValueGroups] = useState({
-    hours: "10",
-    minutes: "0",
+    hours: "11",
+    minutes: "00",
     time: "AM",
   });
 
@@ -31,15 +35,49 @@ function BookingInfo() {
     }));
   };
 
+  const next = () => {
+    if (selectRef.current.value == "0") {
+      generateToast("Please select a place.", "error");
+      return;
+    }
+
+    console.log(valueGroups);
+
+    if (valueGroups.hours < "11" && valueGroups.time === "AM") {
+      generateToast("Unfortunatly, Dice is closed before 11AM", "error");
+      return;
+    } else if (
+      valueGroups.hours >= "10" &&
+      valueGroups.time === "PM" &&
+      valueGroups.minutes !== "00"
+    ) {
+      generateToast("Unfortunatly, Dice is closed after 10PM", "error");
+      return;
+    }
+
+    updateData("placeId", selectRef.current.value);
+    updateData("nbPeople", nbPeople);
+    updateData("time", valueGroups);
+    goForward();
+  };
+
   return (
     <>
       <div className={styles.container}>
+        <button className={styles.backButton} onClick={goBack}>
+          &lt;-- Go Back
+        </button>
         <div className={styles.place}>
           <h2 htmlFor="place" className={styles.label}>
             Place
           </h2>
-          <select name="place" id="place" className={styles.select}>
-            <option value="Select Place">Select Place</option>
+          <select
+            name="place"
+            id="place"
+            className={styles.select}
+            ref={selectRef}
+          >
+            <option value="0">Select Place</option>
             <option value="1">Portsmouth</option>
             <option value="2">Worthing</option>
           </select>
@@ -72,7 +110,9 @@ function BookingInfo() {
             </button>
           </div>
         </div>
-        <div className={styles.submitButton}>Next</div>
+        <div className={styles.submitButton} onClick={() => next()}>
+          Next
+        </div>
       </div>
     </>
   );
