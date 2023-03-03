54e7fd194fe4ee360cd0aee078a8a62b60601efc
JulesEfrei <jules.bruzeau@efrei.net>
2023-03-03 18:33:41 +0000

    Add : test to import font

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./utils/fonts.css";
+import "./assets/fonts/Liberator.ttf";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
