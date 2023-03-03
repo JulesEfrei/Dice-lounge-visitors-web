826330b1935bf158faac2e22433865a94914527f
JulesEfrei <jules.bruzeau@efrei.net>
2023-03-03 14:11:14 +0000

    Add : Styles on page 2

--- src/components/Sections/BookingInfo.jsx
+++ src/components/Sections/BookingInfo.jsx
@@ -3,6 +3,8 @@ import styles from "./styles/bookingInfo.module.scss";
 import Picker from "react-mobile-picker-mod";
 
 function BookingInfo() {
+  const [nbPeople, setNbPeople] = useState(2);
+
   const hours = new Array(12)
     .fill(0, 0)
     .map((elm, index) => (index + 1).toString());
@@ -33,20 +35,44 @@ function BookingInfo() {
     <>
       <div className={styles.container}>
         <div className={styles.place}>
-          <label htmlFor="place">Place</label>
-          <select name="place" id="place">
+          <h2 htmlFor="place" className={styles.label}>
+            Place
+          </h2>
+          <select name="place" id="place" className={styles.select}>
             <option value="Select Place">Select Place</option>
             <option value="1">Portsmouth</option>
             <option value="2">Worthing</option>
           </select>
         </div>
-        <div className={styles.people}>
+        <div className={styles.time}>
+          <h2 className={styles.text}>Pick a time</h2>
           <Picker
             optionGroups={optionGroups}
             valueGroups={valueGroups}
             onChange={handleChange}
           />
         </div>
+        <div className={styles.people}>
+          <h2 className={styles.title}>Number of people</h2>
+          <div className={styles.wrapper}>
+            <button
+              className={styles.button}
+              onClick={() => setNbPeople((curr) => curr - 1)}
+              disabled={nbPeople === 2 ? true : false}
+            >
+              -
+            </button>
+            <h3 className={styles.count}>{nbPeople}</h3>
+            <button
+              className={styles.button}
+              onClick={() => setNbPeople((curr) => curr + 1)}
+              disabled={nbPeople === 10 ? true : false}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <div className={styles.submitButton}>Next</div>
       </div>
     </>
   );
